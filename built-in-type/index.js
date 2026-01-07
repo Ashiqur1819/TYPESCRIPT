04c677ea851164c00e85b11957c2b258f6645dca
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-08 01:24:10 +0600

    Done built in data type

---
+++ built-in-type/index.js
@@ -0,0 +1,10 @@
+// Built in data type: number, string, boolean, null, undefined, void etc...
+var userId;
+var userName;
+var age;
+var isMarried;
+userId = 101;
+userName = "Nusrat Jahan";
+age = 18,
+    isMarried = true;
+console.log({ userId: userId, userName: userName, age: age, isMarried: isMarried });
---
+++ built-in-type/index.ts
@@ -0,0 +1,12 @@
+// Built in data type: number, string, boolean, null, undefined, void etc...
+
+let userId: number;
+let userName: string;
+let age: number;
+let isMarried: boolean;
+
+userId = 101;
+userName = "Nusrat Jahan";
+(age = 18), (isMarried = true);
+
+console.log({ userId, userName, age, isMarried });
