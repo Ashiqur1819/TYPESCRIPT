f47a5895efa2b08f2362fa87adbae4061e224ef0
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-08 02:19:43 +0600

    Done array type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,3 @@
+var users = ["Ashiq", "Nusrat", "Era"];
+var numbers = [1, 3, 5, 7];
+console.log(users, numbers);
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,6 @@
+const users: string[] = ["Ashiq", "Nusrat", "Era"]
+const numbers: Array<number> = [1, 3, 5, 7]
+
+
+console.log(users, numbers)
+
