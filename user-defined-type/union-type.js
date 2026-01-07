0b75662ff1fa73a6bfe8ad7314c357c951b8db43
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-08 01:37:00 +0600

    Done union type

---
+++ user-defined-type/union-type.js
@@ -0,0 +1,5 @@
+var userInfo;
+var userInfo2;
+userInfo = "Nusrat";
+userInfo2 = 101;
+console.log({ userInfo: userInfo, userInfo2: userInfo2 });
---
+++ user-defined-type/union-type.ts
@@ -0,0 +1,7 @@
+let userInfo: string | number;
+let userInfo2: string | number;
+
+userInfo = "Nusrat";
+userInfo2 = 101;
+
+console.log({userInfo, userInfo2})
